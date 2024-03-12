094d863701b7152d1fa87f86ddccfd6fe5e5c879
rohith-sura <surarohith97@gmail.com>
2024-03-11 23:51:32 -0400

    Initial Commit

---
+++ src/test/java/com/fitnesslog/FitnessLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
